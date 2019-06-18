0dc5210af349a13fb6f86a0d5bbea75c5afc31fd
Syalcindag <40965702+Syalcindag@users.noreply.github.com>
2019-06-18 11:41:35 +0300

    ✔

---
+++ resources/views/okul/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layout/template')
+
+@section('content')
+ <h1>Okul Tablosu</h1>
+ <a href="{{url('/books/create')}}" class="btn btn-success">Yeni Kayıt</a>
+ <hr>
+ <table class="table table-striped table-bordered table-hover">
+     <thead>
+     <tr class="bg-info">
+         <th>No</th>
+         <th>Ad</th>
+         <th>Soyad</th>
+
+
+         <th colspan="2">Actions</th>
+     </tr>
+     </thead>
+     <tbody>
+     @foreach ($okul as $kayit)
+         <tr>
+             <td>{{ $kayit->no }}</td>
+             <td>{{ $kayit->ad }}</td>
+             <td>{{ $kayit->soyad }}</td>
+
+
+
+             <td><a href="{{route('okul.edit',$kayit->no)}}" class="btn btn-warning">Update</a></td>
+             <td>
+             {!! Form::open(['method' => 'DELETE', 'route'=>['okul.destroy', $kayit->no]]) !!}
+             {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+             {!! Form::close() !!}
+             </td>
+         </tr>
+     @endforeach
+
+     </tbody>
+
+ </table>
+ <?php echo $okul->links(); ?>
+@endsection
